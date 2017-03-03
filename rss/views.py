e971974d4929a9a7492a89844d454552c7765a4c
nixon <nixon613@gmail.com>
2017-03-03 20:17:14 +0600

    get parsed data

--- rss/views.py
+++ rss/views.py
@@ -9,13 +9,17 @@ import feedparser
 class getRssDetails(APIView):
     def get(self, request, format=None):
 
-        # parsed_data = feedparser.parse('http://feeds.feedburner.com/300mbfilms1')
-        parsed_data = feedparser.parse('http://www.feedforall.com/blog-feed.xml')
+        parsed_data = feedparser.parse('http://feeds.feedburner.com/300mbfilms1')
+        # parsed_data = feedparser.parse('http://www.feedforall.com/blog-feed.xml')
 
         # print(parsed_data)
 
         return JsonResponse({
             'success': True,
-            'rss_data': parsed_data
+            'name':parsed_data['feed']['title'],
+            'subtitle':parsed_data['feed']['subtitle'],
+            'link':parsed_data['feed']['link'],
+            'updated':parsed_data['feed']['updated'],
+            'entries':parsed_data['entries'],
         }, safe=False)
 
